b3b6abec856fdd0667955536faceb0041e0d7d49
Manuel M <mamueluth@gmail.com>
2022-11-16 10:44:21 +0100

    Start first rrbot-satellite in /sub_1 namespace

--- ros2_control_demo_bringup/launch/two_distributed_rrbots/two_distributed_rrbots.launch.py
+++ ros2_control_demo_bringup/launch/two_distributed_rrbots/two_distributed_rrbots.launch.py
@@ -14,15 +14,37 @@
 
 
 from launch import LaunchDescription
-from launch.actions import RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable,LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "namespace",
+            default_value="/",
+            description="Start everything relative to provided namespace.",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controller_manager_name",
+            default_value="/controller_manager",
+            description="Fullname of controller_manager. Set if you define the namespace yourself.",
+        )
+    )
+
+    # Initialize Arguments
+    namespace = LaunchConfiguration("namespace")
+    controller_manager_name = LaunchConfiguration("controller_manager_name")
+
     # Get URDF via xacro
     robot_description_content = Command(
         [
@@ -46,14 +68,24 @@ def generate_launch_description():
             "two_distributed_rrbots_central_controller.yaml",
         ]
     )
+
+    robot_controllers_satellite_1 = PathJoinSubstitution(
+        [
+            FindPackageShare("ros2_control_demo_bringup"),
+            "config/two_distributed_rrbots",
+            "rrbot_controllers_satellite_1.yaml",
+        ]
+    )
+    
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("rrdiffbot_description"), "config", "rrdiffbot.rviz"]
+        [FindPackageShare("two_distributed_rrbots_description"), "config", "two_distributed_rrbots.rviz"]
     )
 
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
+        namespace=namespace,
+        parameters=[robot_description, robot_controllers_satellite_1],
         remappings=[
             (
                 "/forward_position_controller/commands",
@@ -65,6 +97,7 @@ def generate_launch_description():
     robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
+        namespace=namespace,
         output="both",
         parameters=[robot_description],
     )
@@ -72,6 +105,7 @@ def generate_launch_description():
         package="rviz2",
         executable="rviz2",
         name="rviz2",
+        namespace=namespace,
         output="log",
         arguments=["-d", rviz_config_file],
     )
@@ -79,13 +113,15 @@ def generate_launch_description():
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        namespace=namespace,
+        arguments=["joint_state_broadcaster", "-c", controller_manager_name],
     )
 
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["forward_position_controller", "-c", "/controller_manager"],
+        namespace=namespace,
+        arguments=["forward_position_controller", "-c", controller_manager_name],
     )
 
     # Delay rviz start after `joint_state_broadcaster`
@@ -112,4 +148,4 @@ def generate_launch_description():
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
     ]
 
-    return LaunchDescription(nodes)
+    return LaunchDescription(declared_arguments + nodes)
